5c8bc0c014f3ddd502e516fbc445d30a782abf3c
Yeah <yeah@gmail.com>
2024-11-17 11:58:25 -0800

    chore(frontend): add table of todo

---
+++ client/src/components/TableHeader.jsx
@@ -0,0 +1,15 @@
+import { SorttopIcon, SortdownIcon } from "./Icons";
+
+const TableHeader = (props) => {
+  return (
+    <div className="flex gap-[5px] items-center">
+      <p className="text-20 text-[#2CA1C2] ">{props.name}</p>
+      <div className="space-y-[-8px]">
+        <SorttopIcon />
+        <SortdownIcon />
+      </div>
+    </div>
+  );
+};
+
+export default TableHeader;
--- client/src/pages/todo/index.jsx
+++ client/src/pages/todo/index.jsx
@@ -1,21 +1,101 @@
+import { SearchIcon } from "../../components/Icons";
+import TableHeader from "../../components/TableHeader";
+import ButtonArray from "../../components/ButtonArray";
+
 const Todo = () => {
   return (
-    <div className="main">
-      <div className="list">
-        <div>priority</div>
-        <div>date</div>
-        <div>completion</div>
+    <div className="px-[10px] md:px-[80px] lg:px-[160px] py-[50px] space-y-[35px]">
+      <div className="flex items-center pl-[20px] py-[10px] border-[1px] border-gray-40 w-[350px] h-[50px] rounded-[10px] gap-[15px] active:border-blue-500 hover:border-blue-500">
+        <SearchIcon />
+        <input
+          type="text"
+          placeholder="Search ..."
+          className="border-none text-[18px] text-gray-500 outline-none"
+        />
       </div>
-      <table className="table">
+      <table>
         <tr>
-          <th>priority</th>
-          <th>timeline</th>
-          <th>description</th>
-          <th>completion</th>
+          <th>
+            <TableHeader name="PRIORITY" />
+          </th>
+          <th>
+            <TableHeader name="TIMELINE" />
+          </th>
+          <th>DESCRIPTION</th>
+          <th>
+            <TableHeader name="COMPLETION" />
+          </th>
+          <th className="text-center">ACTION</th>
+        </tr>
+        <tr>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td>
+            <ButtonArray />
+          </td>
+        </tr>
+        <tr className="py-20px">
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td>
+            <ButtonArray />
+          </td>
+        </tr>
+        <tr>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td>
+            <ButtonArray />
+          </td>
+        </tr>
+        <tr>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td className="text-15px" text-white>
+            ddddd
+          </td>
+          <td>
+            <ButtonArray />
+          </td>
         </tr>
       </table>
-      <div>dsakfjwaepfjweipofujewqpoifjwaeipofjwaepoifjwaepoifjwaepoif</div>
-      <div>dsi;opoxczivjsdgiofioedfioefio</div>
     </div>
   );
 };
