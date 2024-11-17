26a6612a209e549f3fea2d20edb2f4c69c2b024f
spaceman <space@spa.com>
2024-11-16 19:31:03 -0800

    fix(frontend): fix bug

--- client/src/pages/todo/index.jsx
+++ client/src/pages/todo/index.jsx
@@ -15,6 +15,7 @@ const Todo = () => {
         </tr>
       </table>
       <div>dsakfjwaepfjweipofujewqpoifjwaeipofjwaepoifjwaepoifjwaepoif</div>
+      <div>dsi;opoxczivjsdgiofioedfioefio</div>
     </div>
   );
 };
