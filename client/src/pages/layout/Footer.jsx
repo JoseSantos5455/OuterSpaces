0886b0e361544ce4990beee0db0f9a2730f46cfc
spaceman <space@spa.com>
2024-11-16 18:00:31 -0800

    feat(frontend): add header

---
+++ client/src/pages/layout/Footer.jsx
@@ -0,0 +1,27 @@
+import { Instagram, Facebook, Twitter, Telegram } from "../../component/icon";
+
+const Footer = () => {
+  return (
+    <div className="footer mx-[10px] md:mx-[80px] lg:mx-[160px] block md:flex">
+      <div className="py-[24px]">
+        Copyright © 2021 Tronix. All Right Reseved
+      </div>
+      <div className="footer-contact">
+        <button style={{ backgroundColor: "#8bbff0" }}>
+          <Instagram />
+        </button>
+        <button style={{ backgroundColor: "#8bbff0" }}>
+          <Twitter />
+        </button>
+        <button style={{ backgroundColor: "#8bbff0" }}>
+          <Facebook />
+        </button>
+        <button style={{ backgroundColor: "#8bbff0" }}>
+          <Telegram />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/pages/layout/Header.jsx
+++ client/src/pages/layout/Header.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import "../style.css";
+
 const Header = () => {
   return (
-    <div>
-      <div>logo</div>
+    <div className="flex justify-between items-center px-[10px] md:px-[80px] lg:px-[160px] pt-4 h-[120px] bg-blue-100 bg-opacity-20">
+      <Link>
+        <div className="flex justify-center items-center gap-4 text-[36px]">
+          <img src="img/logo192.png" alt="logo" className="w-10 h-10" />
+          <p>TO-DO</p>
+        </div>
+      </Link>
+      <div className="flex justify-center items-center gap-[10px] text-[18px]">
+        <button className="header-link">Main</button>
+        <div className=" flex justify-center items-center gap-[10px]">
+          <button className="header-btn h-[40px] bg-blue-100">Sign In</button>
+          <button className="header-btn h-[40px] bg-blue-100">Sign Up</button>
+        </div>
+      </div>
     </div>
   );
 };
