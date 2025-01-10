ca1e5f6a3d4b18f727eecbf64835feed49756102
SatoruSaito <satorusaito1108@gmail.com>
2025-01-10 13:57:53 -0800

    fix(frontend): update bug

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -6,6 +6,7 @@ const initialState = {
   isLoggedIn: false,
   login: () => {},
   logout: () => {},
+  logout: () => {},
 };
 
 // ==============================|| CONFIG CONTEXT & PROVIDER ||============================== //
